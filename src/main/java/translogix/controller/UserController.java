f28a7ba55e61aeed871995b117643d77d0dfdd05
Radu Nastas <nastas799@gmail.com>
2025-01-03 20:02:31 +0100

    TRL-15: Prepared Translogix POC

---
+++ src/main/java/translogix/controller/UserController.java
@@ -0,0 +1,26 @@
+package translogix.controller;
+
+import translogix.entity.User;
+import translogix.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
---
+++ src/main/java/translogix/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package translogix.repository;
+
+import translogix.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
